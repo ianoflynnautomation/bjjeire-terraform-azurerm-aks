25677ad9b844d3e47a54b6178ac33589ac7151d0
ianoflynn <ian.oflynn11@gmail.com>
2026-04-11 18:11:35 +0200

    fix: format

--- variables.tf
+++ variables.tf
@@ -2784,3 +2784,20 @@ variable "cluster_domain" {
 Defaults to cluster.local.
 DESCRIPTION
 }
+
+variable "storage_images_account_name" {
+  type        = string
+  description = "Name of the storage account for gym/event images. Globally unique, 3-24 lowercase alphanumeric chars."
+}
+
+variable "storage_images_replication_type" {
+  type        = string
+  description = "Replication type for the images storage account (LRS, ZRS, GRS)."
+  default     = "LRS"
+}
+
+variable "storage_images_cors_origins" {
+  type        = list(string)
+  description = "Allowed CORS origins for blob reads. Set to your Cloudflare-proxied domain(s) in production."
+  default     = ["*"]
+}
